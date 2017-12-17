f7d306da17edff54a0ed3c0e968ce946c093393b
AntoConn <g00347577@gmit.ie>
2017-12-17 03:01:41 +0000

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -23,24 +23,92 @@ namespace connect4
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int turnCount = 1;
+        int collNo=0;
+        int rowNo1=-0,rowNo2 = 0,rowNo3 = 0,rowNo4 = 0, rowNo5 = 0, rowNo6 = 0;
+        Ellipse piece;
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-
-        private void MouseEnter(object sender, Windows.Devices.Input.MouseEventArgs e)
+        private void PointrIn(object sender, PointerRoutedEventArgs e)
         {
-            // ... Change Ellipse to a blue color.
             var ellipse = sender as Ellipse;
-            ellipse.Fill = new SolidColorBrush(Windows.UI.Colors.Yellow);
+            if (turnCount % 2 == 0)
+            {
+                ellipse.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+            }
+            else
+            {
+                ellipse.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+            }
         }
 
-        private void Ellipse_MouseLeave(object sender, Windows.Devices.Input.MouseEventArgs e)
+        private void PointrOut(object sender, PointerRoutedEventArgs e)
         {
-            // ... Change Ellipse to a red color.
             var ellipse = sender as Ellipse;
-            ellipse.Fill = new SolidColorBrush(Windows.UI.Colors.White); ;
+            ellipse.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
         }
+
+        private void Clicked(object sender, PointerRoutedEventArgs e)
+        {
+            turnCount++;
+            
+        }
+
+        private void Clicked1(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 2;
+            rowNo1 = rowNo1 + 2;
+            if (turnCount % 2 == 0)
+            {
+                C2R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+            }
+            else
+            {
+                C2R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+            }
+            turnCount++;
+
+        }
+
+        private void Clicked2(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 3;
+            turnCount++;
+        }
+
+        private void Clicked3(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 5;
+            turnCount++;
+        }
+
+        private void Clicked4(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 7;
+            turnCount++;
+        }
+
+        private void Clicked5(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 9;
+            turnCount++;
+        }
+
+        private void Clicked6(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 11;
+            turnCount++;
+        }
+
+        private void Clicked7(object sender, PointerRoutedEventArgs e)
+        {
+            collNo = 13;
+            turnCount++;
+        }
+
+
     }
 }
