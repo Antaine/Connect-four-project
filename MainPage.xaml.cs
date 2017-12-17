57e1698349eefa9e10e99dc52c533e4dab149e79
AntoConn <g00347577@gmit.ie>
2017-12-17 18:15:44 +0000

    Add files via upload
    Allows all the pieces to be entered

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -25,7 +25,7 @@ namespace connect4
     {
         int turnCount = 1;
         int collNo=0;
-        int rowNo1=-0,rowNo2 = 0,rowNo3 = 0,rowNo4 = 0, rowNo5 = 0, rowNo6 = 0;
+        int rowNo1=-0,rowNo2 = 0,rowNo3 = 0,rowNo4 = 0, rowNo5 = 0, rowNo6 = 0,rowNo7 =0;
         Ellipse piece;
         public MainPage()
         {
@@ -59,16 +59,87 @@ namespace connect4
 
         private void Clicked1(object sender, PointerRoutedEventArgs e)
         {
-            collNo = 2;
+            collNo = 1;
             rowNo1 = rowNo1 + 2;
-            if (turnCount % 2 == 0)
+            if(rowNo1 == 2)
             {
-                C2R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                if (turnCount % 2 == 0)
+                {
+                    C1R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C1R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
             }
+
+           else if (rowNo1 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C1R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C1R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+           else if (rowNo1 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C1R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C1R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo1 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C1R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C1R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo1 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C1R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C1R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo1 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C1R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C1R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
             else
             {
-                C2R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                enterPiece1.StrokeThickness = 0;
+                enterPiece1.Fill = new SolidColorBrush(color: Windows.UI.Colors.White) ;
+                turnCount--;
             }
+
             turnCount++;
 
         }
@@ -76,36 +147,511 @@ namespace connect4
         private void Clicked2(object sender, PointerRoutedEventArgs e)
         {
             collNo = 3;
+            rowNo2 = rowNo2 + 2;
+
+            if (rowNo2 == 2)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C3R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C3R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo2 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C3R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C3R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo2 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C3R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C3R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo2 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C3R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C3R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo2 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C3R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C3R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo2 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C3R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C3R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else
+            {
+                enterPiece2.StrokeThickness = 0;
+                enterPiece2.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
+                turnCount--;
+            }
             turnCount++;
         }
 
         private void Clicked3(object sender, PointerRoutedEventArgs e)
         {
             collNo = 5;
+            rowNo3 = rowNo3 + 2;
+            if (rowNo3 == 2)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C5R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C5R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo3 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C5R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C5R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo3 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C5R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C5R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo3 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C5R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C5R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo3 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C5R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C5R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo3 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C5R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C5R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else
+            {
+                enterPiece3.StrokeThickness = 0;
+                enterPiece3.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
+                turnCount--;
+            }
             turnCount++;
         }
 
         private void Clicked4(object sender, PointerRoutedEventArgs e)
         {
             collNo = 7;
+            rowNo4 = rowNo4 + 2;
+            if (rowNo4 == 2)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C7R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C7R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo4 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C7R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C7R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo4 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C7R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C7R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo4 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C7R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C7R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo4 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C7R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C7R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo4 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C7R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C7R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else
+            {
+                enterPiece4.StrokeThickness = 0;
+                enterPiece4.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
+                turnCount--;
+            }
             turnCount++;
         }
 
         private void Clicked5(object sender, PointerRoutedEventArgs e)
         {
             collNo = 9;
+            rowNo5 = rowNo5 + 2;
+            if (rowNo5 == 2)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C9R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C9R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo5 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C9R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C9R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo5 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C9R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C9R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo5 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C9R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C9R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo5 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C9R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C9R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo5 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C9R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C9R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else
+            {
+                enterPiece5.StrokeThickness = 0;
+                enterPiece5.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
+                turnCount--;
+            }
             turnCount++;
         }
 
         private void Clicked6(object sender, PointerRoutedEventArgs e)
         {
             collNo = 11;
+            rowNo6 = rowNo6 + 2;
+            if (rowNo6 == 2)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C11R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C11R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo6 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C11R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C11R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo6 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C11R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C11R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo6 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C11R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C11R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo6 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C11R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C11R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo6 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C11R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C11R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else
+            {
+                enterPiece6.StrokeThickness = 0;
+                enterPiece6.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
+                turnCount--;
+            }
             turnCount++;
         }
 
         private void Clicked7(object sender, PointerRoutedEventArgs e)
         {
             collNo = 13;
+            rowNo7 = rowNo7 + 2;
+            if (rowNo7 == 2)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C13R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C13R12.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo7 == 4)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C13R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C13R10.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo7 == 6)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C13R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C13R8.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo7 == 8)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C13R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C13R6.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo7 == 10)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C13R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C13R4.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else if (rowNo7 == 12)
+            {
+                if (turnCount % 2 == 0)
+                {
+                    C13R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Yellow);
+                }
+                else
+                {
+                    C13R2.Fill = new SolidColorBrush(color: Windows.UI.Colors.Red);
+                }
+            }
+
+            else
+            {
+                enterPiece7.StrokeThickness = 0;
+                enterPiece7.Fill = new SolidColorBrush(color: Windows.UI.Colors.White);
+                turnCount--;
+            }
             turnCount++;
         }
 
